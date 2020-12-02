33ed00d04698bdec173d7b0d3de80ab8b171ecec
lubo4kaTarasenko <lubo4ka.boiko@gmail.com>
2020-12-02 14:25:43 +0200

    change letter  & interval

--- src/app/components/square.js
+++ src/app/components/square.js
@@ -11,12 +11,36 @@ export default class Square extends React.Component {
     return (
       <div className='container'> 
         <div className='grow_field'>
-            <div id='score'><b>Score = 100</b></div> 
-            <div id='main'></div>         
+            <div id='score'><b>Score = <span id='numb'>100</span></b></div> 
+            <div id='max'><div id='main'></div></div>        
         </div>
         <div className='task_field'> 
-          <div id='letters'></div>         
+          <div id='letters'></div>    
+          <button className="start" onClick={()=>{this.startGame()}}>Start</button>     
         </div>
       </div>  
-  )}  
-}
+  )}
+  
+  returnRandomLetter(){
+    var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", 
+                  "T", "U", "V", "W", "X", "Y", "Z"]
+    var letter = letters[Math.floor(Math.random()*letters.length)];
+    return letter
+  }
+
+  putLetter(){
+    const letter = this.returnRandomLetter()
+    const lettersCont = document.getElementById('letters')
+    lettersCont.innerHTML = `<b>${letter}</b>`
+  }
+
+  startGame(){
+    //let timerId = 
+    setInterval(() => this.putLetter(), 3000);
+    //setTimeout(() => { clearInterval(timerId); alert('stop'); }, 5000);    
+  }
+
+
+
+
+} 
