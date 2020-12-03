69d1e00a34f61a85cd3039aacc167a50a1bb0f2f
lubo4kaTarasenko <lubo4ka.boiko@gmail.com>
2020-12-03 17:26:53 +0200

    react way, progress bar, game logic

--- src/app/components/square.js
+++ src/app/components/square.js
@@ -4,6 +4,9 @@ export default class Square extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      count: 100,
+      letter: '',
+      progress: 0
      // square = []
     }
   }
@@ -11,54 +14,77 @@ export default class Square extends React.Component {
     return (
       <div className='container'> 
         <div className='grow_field'>
-            <div id='score'><b>Score = <span id='numb'>100</span></b></div> 
+            <div id='score'><b>Score = <span id='numb'>{this.state.count}</span></b></div> 
             <div id='max'><div id='main'></div></div>        
         </div>
         <div className='task_field'> 
-          <div id='letters'></div>    
-          <button className="start" onClick={()=>{this.startGame()}}>Start</button>     
+          <div id='letters'>{this.state.letter}</div>
+          <p><progress id="progress" max="60" value={this.state.progress} 
+          style={ { opacity: (this.state.letter ? 100 : 0)} } /></p>   
+          <p><button className="start" onClick={()=>{this.startGame()}}>Start</button></p>     
         </div>
       </div>  
   )}
   
-  returnRandomLetter(){
+  putRandomLetter(){
     var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", 
                   "T", "U", "V", "W", "X", "Y", "Z"]
     var letter = letters[Math.floor(Math.random()*letters.length)];
-    return letter
+    this.setState({letter: letter, progress: 0 })
+    if (this.progressTimer){
+      clearInterval(this.progressTimer)
+      this.progressTimer = null
+    }
+    this.progressTimer = setInterval(() => this.showProgress(), 500);
   }
 
-  putLetter(letter){
-    const lettersCont = document.getElementById('letters')
-    lettersCont.innerHTML = `<b>${letter}</b>`
-    this.isLetterEq(letter)
+  componentDidMount() {
+    this.isLetterEq()
   }
 
-  isLetterEq(letter){
-    document.addEventListener('keydown', function(event) {
-      if (event.code === `Key${letter}`) this.countUp()
+  isLetterEq(){
+    document.addEventListener('keydown', (event) => {
+      event.code === `Key${this.state.letter}` ? this.countUp() : this.countDown();
     })
   }
 
   countUp(){
     const points = 10
-    const count = document.getElementById('numb')
-    let countVal = count.value
-    count.innerHTML = `${countVal += points}`
+    const count = this.state.count
+    const newScore = points + count;
+    this.setState({ count: newScore, letter: '' });
+    this.checkScore(newScore)
   }
 
   countDown(){
-
+    const points = 20
+    const count = this.state.count
+    const newScore = count - points;
+    this.setState({count: newScore, letter: ''});
+    this.checkScore(newScore)
   }
 
+  checkScore(score) {
+    if(score >= 200) {
+      clearInterval(this.timerId)
+      alert('Congrats!');
+    }
+
+    if(score <= 0) {
+      clearInterval(this.timerId)
+      alert('You are lost!');
+    }
+  }
+
+  showProgress(){
+    const progress = this.state.progress
+    let newProgress = progress + 10
+    if (progress > 60) newProgress = 0
+    this.setState({progress: newProgress});
+  }
 
   startGame(){
-    //let timerId = 
-    setInterval(() => this.putLetter(this.returnRandomLetter()), 3000);
-    //setTimeout(() => { clearInterval(timerId); alert('stop'); }, 5000);    
+    this.putRandomLetter() 
+    this.timerId =  setInterval(() => this.putRandomLetter(), 3000); 
   }
-
-
-
-
 } 
